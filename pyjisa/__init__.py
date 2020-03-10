0302aeb79fba3012c5c82f56eab6333cd770d7b4
William Wood <waw31@cam.ac.uk>
2020-03-10 16:07:16 +0000

    path

--- pyjisa/__init__.py
+++ pyjisa/__init__.py
@@ -1,9 +1,12 @@
 # Import JPype
+import os
 import jpype
 import jpype.imports
 
+path = os.path.dirname(os.path.realpath(__file__))
+
 # Link in JISA.jar classes
-jpype.addClassPath("./JISA.jar")
+jpype.addClassPath(os.path.join(path, "JISA.jar"))
 jpype.imports.registerDomain("jisa")
 
 # Start the JVM
